1863a3221ce49dcf81f1d5f05264c91abc651fd0
Olivier Cortès <olive@licorn.org>
2008-12-22 18:12:03 +0100

    [tailoritself @ olive@deep-ocean.net-20081222171203-0jsq3hsyxcw6flv2]
    Ignore-this: 6c1ca63d7ee3bae095e307d89d221b94
    licorn.foundations.objects: add classes LicornThread and StateMachine.
    
    darcs-hash:20081222171203-900c4-db9376ecaa95055692b874786f88b4643b9ac2f0

--- foundations/objects.py
+++ foundations/objects.py
@@ -8,4 +8,75 @@ Copyright (C) 2005-2008 Olivier Cortès <olive@deep-ocean.net>
 Licensed under the terms of the GNU GPL version 2
 
 """
+from Queue              import Queue
+from threading          import Thread, Event
+from licorn.foundations import exceptions
 
+
+class LicornThread(Thread) :
+	"""
+		A simple thread with an Event() used to stop it properly, and a Queue() to
+		get events from other threads asynchronically.
+	"""
+
+	def __init__(self, pname = '<unknown>') :
+		self.name  = str(self.__class__).rsplit('.', 1)[1].split("'")[0]
+		Thread.__init__(self, name = "%s/%s" % (pname, self.name))
+
+		self._stop_event  = Event()
+		self._input_queue = Queue()
+
+	def dispatch_message(msg) :
+		self._input_queue.put(msg)
+
+	def run(self) :
+
+		if callable(getattr(self, 'process_message')) :
+			raise exceptions.LicornRuntimeError("%s: no process_message() method !!" % self.name)
+		
+		logging.progress('%s: started.' % self.name)
+		while not self._stop_event().isSet() :
+			data = self._input_queue.get()
+			if data is None :
+				continue
+			self.process_message(data)
+		logging.progress('%s: stopped.' % self.name)
+	def stop(self) :
+		logging.progress('%s: stopping thread.' % self.name)
+		self._stop_event.set()
+		self._input_queue.put(None)
+
+class StateMachine :
+	"""
+		A Finite state machine design pattern.
+		Found at http://www.ibm.com/developerworks/library/l-python-state.html , thanks to David Mertz.
+	"""
+
+	def __init__(self) :
+		self.handlers = {}
+		self.startState = None
+		self.endStates = []
+
+	def add_state(self, name, handler, end_state = False) :
+		self.handlers[name] = handler
+		if end_state :
+			 self.endStates.append(name)
+
+	def set_start(self, name) :
+		self.startState = name
+
+	def run(self, data) :
+		try :
+			 handler = self.handlers[self.startState]
+		except :
+			 raise exceptions.LicornRuntimeError("LSM: must call .set_start() before .run()")
+
+		if not self.endStates :
+				 raise exceptions.LicornRuntimeError("LSM: at least one state must be an end_state.")
+
+		while True :
+			(newState, data) = handler(data)
+			if newState in self.endStates :
+				break 
+			else :
+				handler = self.handlers[newState]
