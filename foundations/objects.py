2e5f94fb674dd6d24c12d9ff1ef5e6ff74c425cd
Olivier Cortès <olive@licorn.org>
2009-01-09 17:06:40 +0100

    [tailoritself @ olive@deep-ocean.net-20090109160640-p4yf7zmf4an2i9h5]
    Ignore-this: 2cf71cb1aae631064d0be2d22e287868
    more polish on licorn.foundations.objects.LicornThread class.
    
    darcs-hash:20090109160640-900c4-ffc90c3b0c5ae1eb8922e530280377bef015b1e1

--- foundations/objects.py
+++ foundations/objects.py
@@ -10,7 +10,7 @@ Licensed under the terms of the GNU GPL version 2
 """
 from Queue              import Queue
 from threading          import Thread, Event
-from licorn.foundations import exceptions
+from licorn.foundations import exceptions, logging
 
 class Singleton(object) :
 	__instances = {}
@@ -23,29 +23,37 @@ class LicornThread(Thread) :
 	"""
 		A simple thread with an Event() used to stop it properly, and a Queue() to
 		get events from other threads asynchronically.
+
+		Every subclass must implement a process_message() method, which will be called
+		by the main loop until the thread stops.
 	"""
 
 	def __init__(self, pname = '<unknown>') :
 		self.name  = str(self.__class__).rsplit('.', 1)[1].split("'")[0]
 		Thread.__init__(self, name = "%s/%s" % (pname, self.name))
 
+
 		self._stop_event  = Event()
 		self._input_queue = Queue()
-	def dispatch_message(msg) :
+		#logging.progress('%s: thread initialized.' % self.name)
+	def dispatch_message(self, msg) :
+		""" get an incoming message in a generic way. """
 		self._input_queue.put(msg)
 	def run(self) :
+		""" Process incoming messages until stop Event() is set. """
 
-		if callable(getattr(self, 'process_message')) :
-			raise exceptions.LicornRuntimeError("%s: no process_message() method !!" % self.name)
-		
-		logging.progress('%s: started.' % self.name)
-		while not self._stop_event().isSet() :
+		logging.progress('%s: thread started.' % self.name)
+
+		while not self._stop_event.isSet() :
 			data = self._input_queue.get()
-			if data is None :
-				continue
+			if data is None : break
 			self.process_message(data)
-		logging.progress('%s: stopped.' % self.name)
+
+		logging.progress('%s: thread ended.' % self.name)
 	def stop(self) :
+		""" Stop current Thread
+		and put a special None entry in the queue, to be
+		sure that self.run() method exits properly. """
 		logging.progress('%s: stopping thread.' % self.name)
 		self._stop_event.set()
 		self._input_queue.put(None)
