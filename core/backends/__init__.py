f4c5ee53b9a5e5f35724db5dd8f886b2e6066318
Olivier Cortès <olive@licorn.org>
2009-01-12 21:52:54 +0100

    [tailoritself @ olive@deep-ocean.net-20090112205254-e3jw6rlj6ygvv7mt]
    Ignore-this: 21dc9ea8fb432acf82616016c482422b
    create licorn.core.backends.
    
    darcs-hash:20090112205254-900c4-232ad20f1daaedce0d11f0ae6db799df7ccbbc93

---
+++ core/backends/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Licorn Daemon backend abstract class.
+
+Copyright (C) 2007-2009 Olivier Cortès <olive@deep-ocean.net>
+Licensed under the terms of the GNU GPL version 2.
+"""
+
