d8458e30b6701e4bdf48faa3397fb91ef42bce7f
Olivier Cortès <oc@meta-it.fr>
2010-10-05 09:56:37 +0200

    fix overlap in ltrace levels.
    Ignore-this: eb8d0172058099293f18003df18f399d
    
    darcs-hash:20101005075637-a4377-3c3c8747415099970ddee7a76acbcb2ac83e71a4

--- foundations/ltrace.py
+++ foundations/ltrace.py
@@ -32,17 +32,17 @@ trc['logging']       = 0x0000000000000001
 trc['foundations']   = 0x0000000000000002
 trc['objects']       = 0x0000000000000004
 trc['options']       = 0x0000000000000008
-trc['readers']       = 0x000000000000000f
-trc['process']       = 0x0000000000000010
-trc['fsapi']         = 0x0000000000000020
+trc['readers']       = 0x0000000000000010
+trc['process']       = 0x0000000000000020
+trc['fsapi']         = 0x0000000000000040
 
 trc['core']          = 0x000000000000ff00
 trc['configuration'] = 0x0000000000000100
 trc['users']         = 0x0000000000000200
 trc['groups']        = 0x0000000000000400
 trc['profiles']      = 0x0000000000000800
-trc['machines']      = 0x0000000000000f00
-trc['internet']      = 0x0000000000001000
+trc['machines']      = 0x0000000000001000
+trc['internet']      = 0x0000000000002000
 
 trc['backends']      = 0x0000000000ff0000
 trc['ldap']          = 0x0000000000010000
@@ -56,9 +56,9 @@ trc['master']        = 0x0000000100000000
 trc['inotifier']     = 0x0000000200000000
 trc['aclchecker']    = 0x0000000400000000
 trc['cache']         = 0x0000000800000000
-trc['crawler']       = 0x0000000f00000000
-trc['thread']        = 0x0000001000000000
-trc['wmi']           = 0x0000002000000000
+trc['crawler']       = 0x0000001000000000
+trc['thread']        = 0x0000002000000000
+trc['wmi']           = 0x0000004000000000
 
 trc['interfaces']    = 0x00ffff0000000000
 trc['cli']           = 0x0000ff0000000000
@@ -66,7 +66,7 @@ trc['add']           = 0x0000010000000000
 trc['mod']           = 0x0000020000000000
 trc['del']           = 0x0000040000000000
 trc['chk']           = 0x0000080000000000
-trc['get']           = 0x00000f0000000000
+trc['get']           = 0x0000100000000000
 
 
 from os   import getenv
