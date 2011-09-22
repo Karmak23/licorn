03937a402f8e14504fe4dc91c8168aa0829d7bd1
Olivier Cortès <oc@meta-it.fr>
2011-09-22 13:59:00 +0200

    version.py: change the comment text, else it gets updated with the version number, too.
    Ignore-this: 2f2b7a34d76372c1a996f5f6e6e806d4
    
    darcs-hash:20110922115900-558f1-3b69dedce6678dd90f2e6a7b2112650f7b13c34d

--- version.py
+++ version.py
@@ -2,14 +2,13 @@
 """
 Licorn version
 
-This file should contain only variables, which content can be either 'dev+r1058',
-or 'dev+r1058\d+' if the licorn repository is installed in developper mode, or
-whatever content if installed in packaging mode (Debian, Ubuntu, etc).
+This file should contain only variables, which content are set via the Makefile
+or packaging tools. Don't alter it manually.
 
-Just type 'make version' in the repository to update the file.
+Just type 'make darcs_record_prehook' in the repository to update the file.
 
 :copyright: 2005-2010 Olivier Cortès <oc@meta-it.fr>
 :license: GNU GPL version 2
 """
 
-version = 'dev+r1060'
+version = 'dev+r1061'
