ca0f2b4e1b5e0c3611b1321e4aea7ff7ad7e5b37
Olivier Cortès <Olivier Corts oc@meta-it.fr>
2012-05-20 07:30:20 +0000

    version.py update for 1.2.5.1

--- version.py
+++ version.py
@@ -2,13 +2,11 @@
 """
 Licorn version
 
-This file should contain only variables, which content are set via the Makefile
-or packaging tools. Don't alter it manually.
-
-Just type 'make darcs_record_prehook' in the repository to update the file.
+This file should contain only variables.
+They must be altered manually, while using the GIT flow tools on a new "release".
 
 :copyright: 2005-2010 Olivier Cortès <oc@meta-it.fr>
 :license: GNU GPL version 2
 """
 
-version = 'dev+r1156'
+version = '1.2.5.1'
