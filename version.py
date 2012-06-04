934dbb45a8ad8efc5da3b43713e2320fa846fa00
Olivier Cortès <olive@licorn.org>
2012-06-04 14:23:09 +0200

    Bump the version number for 1.3 in `version.py` and `docs/conf.py`; comment [very basically] the release process in `version.py`.

--- version.py
+++ version.py
@@ -3,10 +3,20 @@
 Licorn version
 
 This file should contain only variables.
-They must be altered manually, while using the GIT flow tools on a new "release".
+These variables must be altered manually,
+while using the GIT flow tools on a new "release":
+
+	# Example: release_name_and_number == 'licorn-1.3'
+	git flow release start <release_name_and_number>
+
+	# alter the files… do work…
+
+	git flow release finish <release_name_and_number>
+
+	# then package everything for release testing on daily.licorn.org
 
 :copyright: 2005-2010 Olivier Cortès <oc@meta-it.fr>
 :license: GNU GPL version 2
 """
 
-version = '1.3-devel'
+version = '1.3'
