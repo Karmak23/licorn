26f4fecbd068f84af24459c336430a0cb02d9676
Olivier Cortès <olive@licorn.org>
2012-06-16 07:47:42 +0200

    Rewritten and fully tested `wmi.libs.decorators.check_users()`; added a testsuite scenario file, which I all ran manually; this is quite long, but at least are we sure that all checks are made and return the correct result. Next go is the same thing for groups.

--- interfaces/wmi/libs/decorators.py
+++ interfaces/wmi/libs/decorators.py
@@ -57,118 +57,179 @@ def staff_only(func):
 	return decorated
 
 #===================================================== fine grained permissions
-# for more explanations, see http://dev.licorn.org/wiki/LicornPermissions
 def check_users(meta_action, *args, **kwargs):
+	""" Make various checks against "shooting myself or my friends in the foot"
+		classic human errors, which can arise in the WMI because everything
+		is so much easy than in the CLI.
+
+		For detailled informations about what this decorator checks, see
+		the specification: http://dev.licorn.org/wiki/LicornPermissions
+
+		.. note:: the ``rel_id`` (relationships) only exists in the WMI. For
+			more informations, see the
+			:func:`~licorn.interfaces.wmi.users.views.mod` function. Basic
+			syntax for relationships is "<object_id>/<relationship_id>".
+
+		.. versionadded:: 1.3.1. Permissions decorators existed before, but
+			they have been deactivated between the 1.2 and 1.3 development
+			cycles because we had no time to port them.
+"""
 	def decorator(view_func):
 		def decorated(request, *args, **kwargs):
 
 			from licorn.interfaces.wmi.app import wmi_event_app
-			
+
 			q = wmi_event_app.queue(request)
 
-			victim      = LMC.users.by_uid(kwargs.get('uid'))
-			wmi_user    = LMC.users.by_login(request.user.username)
-			admin_group = LMC.groups.by_name(licorn_settings.defaults.admin_group)
-			wmi_group   = LMC.groups.by_name(licorn_settings.licornd.wmi.group)
-			restricted_users =  LMC.users.select(filters.SYSTEM_RESTRICTED)
-			if victim in  restricted_users and not request.user.is_superuser:
+			victim            = LMC.users.by_uid(kwargs.get('uid'))
+			wmi_user          = LMC.users.by_login(request.user.username)
+			admin_group       = LMC.groups.by_name(licorn_settings.defaults.admin_group)
+			wmi_group         = LMC.groups.by_name(licorn_settings.licornd.wmi.group)
+			restricted_users  = LMC.users.select(filters.SYSTEM_RESTRICTED)
+			restricted_groups = LMC.groups.select(filters.SYSTEM_RESTRICTED)
+
+			if victim in restricted_users and not request.user.is_superuser:
 				# Even a staff user can't touch restricted account.
 				q.put(utils.notify(_('No operation allowed on restricted '
 										'system accounts.')))
-				return HttpResponse('ABORTED.')
-											
+				return HttpResponseForbidden('ABORTED.')
+
 			if victim == wmi_user:
+				# I'm trying to modify my own account
+
 				if meta_action == 'delete':
 					# I cannot remove my own account
 					q.put(utils.notify(_('I cannot let you delete your own '
 														'account captain!')))
-					return HttpResponse('ABORTED.')
+					return HttpResponseForbidden(_('Insecure operation.'))
 
 				elif meta_action == 'mod':
-
 					# I can do anything except removing my own power
 					if kwargs.get('action') == 'groups':
-						group_id, rel_id = (int(x) for x in kwargs.get('value').split('/'))
-						group            = LMC.groups.by_gid(group_id)
+						group_id, rel_id = (int(x) for x in
+											kwargs.get('value').split('/'))
 
-						if request.user.is_superuser:
-
-							# If I'm a superuser, I cannot become a standard user
-							# FIXME: WHERE TO FIND DOCUMENTATION ABOUT REL_ID ??
-							if rel_id == 0 and group == admin_group:
-								q.put(utils.notify(_('You tried to remove '
-													'your own account from the '
-													'<em>{0}</em> group. '
-													'<strong>Operation '
-													'aborted</strong>.').format(
-														admin_group.name)))
-								return HttpResponse('ABORTED.')
-
-						elif request.user.is_staff:
-							if rel_id == 0 and group.is_privilege:
-								q.put(utils.notify(_('You tried to remove '
-										'your own account from a privileged '
-										'group. <strong>Operation '
-										'aborted</strong>.')))
-								return HttpResponse('ABORTED.')
+						if request.user.is_staff:
+							if LMC.groups.by_gid(group_id).is_privilege \
+										and rel_id == relation.NO_MEMBERSHIP:
+								q.put(utils.notify(_(u'You tried to remove '
+													u'your own account from a '
+													u'privileged group. '
+													u'<strong>Operation '
+													u'aborted</strong>.')))
+								return HttpResponseForbidden(
+													_('Insecure operation.'))
 
 					elif kwargs.get('action') == 'lock':
-						q.put(utils.notify(_('You tried to lock your own '
-											'account.<strong>Operation '
-											'aborted</strong>.')))
-						return HttpResponse('ABORTED.')
+						q.put(utils.notify(_(u'You tried to lock your own '
+											u'account. <strong>Operation '
+											u'aborted</strong>.')))
+						return HttpResponseForbidden(_('Insecure operation.'))
+
 			else:
-				admins_members = set(admin_group.members + wmi_group.members)
+				# The victim is not myself but another account. We can't delete
+				# or lock them, nor change their password or gecos (anything,
+				# in fact). Only group modifications are handled later.
 
+				if meta_action == 'delete' or (meta_action == 'mod'
+										and kwargs.get('action') != 'groups'):
+
+					# Check if the victim is some kind of admin account.
+					# If then, the user cannot do anything on sibling accounts,
+					# only on lower: eg `admins` can alter `licorn-wmi` but not
+					# other `admins`, and `licorn-wmi` can alter only standard
+					# user accounts.
+
+					if request.user.is_superuser:
+						# `admins` users can do anything, but we prefer them to
+						# battle in CLI. WMI is a peaceful place.
+						if victim in admin_group.members:
+							q.put(utils.notify(_(u'You tried to alter an '
+								u'account as powerful as yours. '
+								u'<strong>Operation aborted</strong>.<br />'
+								u'If you really want to do this, run this '
+								u'operation from a CLI command.')))
+							return HttpResponseForbidden(_('Operation aborted.'))
+
+					elif request.user.is_staff:
+						# if he is  >=  myself, I cannot do anything
+						if victim in admin_group.members \
+												or victim in wmi_group.members:
+							q.put(utils.notify(_(u'You tried to do something on '
+								u'an account as powerful (or even more) than '
+								u'you are. <strong>Operation aborted</strong>.')))
+							return HttpResponseForbidden(_('Operation aborted.'))
+
+			# Whatever the victim is (me or another user), we check if we
+			# are not adding/removing him from unwanted groups.
+			if meta_action == 'mod' and kwargs.get('action') == 'groups':
+				group_id, rel_id = (int(x) for x in
+									kwargs.get('value').split("/"))
+
+				# Cannot remove any `admins` account from the `admins`
+				# group, even if we are `admins` ourselves.
 				if request.user.is_superuser:
-					if victim in admin_group.members:
-						q.put(utils.notify(_('You tried to alter an account '
-							'as powerful as yours. <strong>Operation '
-							'aborted</strong>.<br />If you really want to do '
-							'it, run this operation from a CLI command.')))
-						return HttpResponse('ABORTED.')
+					if group_id == admin_group.gidNumber and \
+											rel_id == relation.NO_MEMBERSHIP:
+						q.put(utils.notify(_(u'You tried to remove {0} '
+									u'from the <em>{1}</em> group. '
+									u'<strong>Operation aborted</strong>.'
+									u'<br /> If you really want to do '
+									u'this, run this operation from a '
+									u'CLI command.').format(
+								_(u'you own account') if victim == wmi_user
+								else _(u'an account as powerful as you are'),
+								admin_group.name)))
+						return HttpResponseForbidden(_('Operation aborted.'))
 
-				# if he is  >=  myself, I cannot do anything
+				# `staff` (eg. `licorn-wmi`) accounts cannot
+				# operate on restricted system groups nor
+				# promote / demote `admins`.
 				elif request.user.is_staff:
-					if victim in admins_members:
-						q.put(utils.notify(_('You tried to do something on '
-							'an account as powerful (or even more) than '
-							'you are. <strong>Operation aborted</strong>.')))
-						return HttpResponse('ABORTED.')
 
-			# check if we are not adding/removing users from unwanted group
-			if meta_action == 'mod':
-				if kwargs.get('action') == 'groups':
-					# syntax value = "1000/1" => user_id/relation_id 
-					#(see users.views.mod)
-					gid, rel = kwargs.get('value').split("/")
+					# if he is >=  myself, I cannot do anything
+					if victim in admin_group.members:
+						q.put(utils.notify(_(u'You tried to do something on '
+											u'an account more powerful than '
+											u'you are. <strong>Operation '
+											u'aborted</strong>.')))
+						return HttpResponseForbidden(_('Not enough permissions.'))
 
-					# cannot remove an admins account from the admins group
-					# even if we are admins.
-					if wmi_user in admin_group.members and \
-						LMC.groups.by_gid(gid) == admin_group and \
-						rel == relation.NO_MEMBERSHIP:
-							q.put(utils.notify(_('You tried to remove an '
-								'account as powerful as you are from the '
-								'<em>{0}<em> group. <strong>Operation aborted'
-								'</strong>. If you really want to do '
-								'it, run this operation from a CLI command.')))
-							return HttpResponse('ABORTED.')
-									
-					# if staff, cannot do anything on restricted groups	
-					elif wmi_user in wmi_group.members and \
-						LMC.groups.by_gid(gid) in set(LMC.groups.select(
-							filters.SYSTEM_RESTRICTED) + [admin_group]):
+					elif group_id == wmi_group.gidNumber and \
+											rel_id == relation.NO_MEMBERSHIP:
+						q.put(utils.notify(_(u'You tried to remove {0} '
+											u'from the <em>{1}</em> group. '
+											u'<strong>Operation '
+											u'aborted</strong>.').format(
+												_(u'you own account')
+													if victim == wmi_user
+													else _(u'an account as '
+															u'powerful (or even '
+															u'more) as you are'),
+												wmi_group.name)))
+						return HttpResponseForbidden(_('Not enough permissions.'))
 
-								q.put(utils.notify(_('You tried to do something on '
-									'a <em>restricted group</em>. '
-									'<strong>Operation aborted</strong>.')))
-								return HttpResponse('ABORTED.')
+					else:
+						group = LMC.groups.by_gid(group_id)
+
+						if group == admin_group	or (
+												group in restricted_groups
+												and not group.is_privilege):
+
+							q.put(utils.notify(_(u'You tried to do something on '
+											u'a <em>restricted group</em> or the '
+											u'<em>{0}</em>. <strong>Operation '
+											u'aborted</strong>.')))
+							return HttpResponseForbidden(
+												_('Not enough permissions.'))
 
 			return view_func(request, *args, **kwargs)
 		return decorated
 	return decorator
 def check_groups(meta_action, *args, **kwargs):
+	""" For more informations, see
+		:func:`~licorn.interfaces.wmi.libs.decorators.check_users`. """
+
 	def decorator(view_func):
 		def decorated(request, *args, **kwargs):
 
@@ -180,7 +241,7 @@ def check_groups(meta_action, *args, **kwargs):
 			admin_group = LMC.groups.by_name(licorn_settings.defaults.admin_group)
 			wmi_group   = LMC.groups.by_name(licorn_settings.licornd.wmi.group)
 			admins_members = set(admin_group.members + wmi_group.members)
-			
+
 			if victim in LMC.groups.select(filters.SYSTEM_RESTRICTED) \
 											and not request.user.is_superuser:
 				# Even a staff user can't touch restricted account.
@@ -214,7 +275,7 @@ def check_groups(meta_action, *args, **kwargs):
 			# check if we are not adding/removing admins user from any group
 			if meta_action == 'mod':
 				if kwargs.get('action') == 'users':
-					# syntax value = "1000/1" => user_id/relation_id 
+					# syntax value = "1000/1" => user_id/relation_id
 					#(see groups.views.mod)
 					uid = kwargs.get('value').split("/")[0]
 					if LMC.users.by_uid(uid) in admins_members:
---
+++ tests/WMI_permissions_decorators_tests.md
@@ -0,0 +1,146 @@
+add priv cdrom
+add group grp1,grp2
+add user testw1,testw2 -G licorn-wmi,cdrom,audio
+add user testa1,testa2 -G admins,grp1
+
+# Licorn® part-time admins (licorn-wmi)
+
+	- login testw1
+
+## me
+
+	- mod gecos > OK
+	- mod passwd > OK
+	- mod shell > OK
+
+	- add me grp1 > OK
+	- mod me grp1 > OK
+	- del me grp1 > OK
+
+	- del me licorn-wmi > error priv
+	- del me cdrom > error priv
+	- (del me audio > not visible)
+
+	- del me > error captain
+	- lock me > error lock me
+	- skel > OK
+	- massive del > error captain
+
+## standard users
+
+	- add user test3 (in grp1, gst-grp2) > OK
+
+	- mod test3 gecos > OK
+	- mod test3 passwd > OK
+	- mod test3 shell > OK
+
+	- add test3 grp1 > OK
+	- mod test3 grp1 > OK
+	- del test3 grp1 > OK
+
+	- add test3 cdrom > OK
+	- del test3 cdrom > OK
+
+	- lock test3 > OK
+	- unlock test3 > OK
+
+	- skel test3 > OK
+	- del test3 > OK
+
+	- add test5 > OK
+	- massive del test5 > OK
+
+
+## siblings
+
+	- mod gecos testw2 > error even more powerful
+	- mod passwd testw2 > error even more powerful
+	- mod shell testw2 > error even more powerful
+
+	- add testw2 grp1 > OK
+	- mod testw2 grp1 > OK
+	- del testw2 grp1 > OK
+
+	- remove testw2 cdrom > OK
+	- add testw2 cdrom > OK
+	- remove testw2 licorn-wmi > error remove licorn-wmi
+
+	- del testw2 > error even more
+	- lock testw2 > error even more
+	(CLI: mod user testw2 -l)
+	- unlock testw2 > error even more
+	- skel testw2 > error even more
+	- massive del > error even more
+
+	- add user test4 (in licorn-wmi) > OK
+	- del user test4 > error even more
+
+## more power (admins)
+
+	- mod gecos testa1 > error even more
+	- mod passwd testa1 > error even more
+	- mod shell testa1 > error even more
+
+	- add testa1 grp2 > error even more
+	- mod testa1 grp1 > error even more
+	- del testa1 grp1 > error even more
+
+	- add testa1 cdrom > error even more
+	- remove testa1 licorn-wmi > error even more
+
+	- lock testw2 > error even more
+	- skel testw2 > error even more
+	- del testw2 > error even more
+	- massive del > error even more
+
+# Licorn® Full-time administrators (`admins`)
+
+	- login testa1
+
+## standard users
+
+	- all like `licorn-wmi`
+
+	- add test6 video > OK
+	- del test6 video > OK
+
+## `licorn-wmi` users
+
+	- all like "standard users" for an admin account.
+
+	- del test7 licorn-wmi > OK
+	- massive del > OK
+
+## Siblings
+
+	- mod gecos testa2 > error even more powerful
+	- mod passwd testa2 > error even more powerful
+	- mod shell testa2 > error even more powerful
+
+	- add testa2 grp1 > OK
+	- mod testa2 grp1 > OK
+	- del testa2 grp1 > OK
+
+	### priv restricted
+	- remove testa2 cdrom > OK
+	- add testa2 cdrom > OK
+
+	### restricted
+	- add testa2 audio > OK
+	- remove testa2 audio > OK
+
+	### system priv
+	- add testa2 licorn-wmi > OK
+	- remove testa2 licorn-wmi > OK
+
+	- del testa2 admins > error even more
+
+	- del testa2 > error even more
+	- lock testa2 > error even more
+	(CLI: mod user testa2 -l)
+	- unlock testa2 > error even more
+	- skel testa2 > error even more
+	- massive del > error even more
+
+	- add user testa3 (in admins) > OK
+	- del user testa3 > error even more
