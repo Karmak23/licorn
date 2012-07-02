df97c8fe0a800551cebbfbc6a3c80cfbcfb882f7
Olivier Cortès <olive@licorn.org>
2012-07-02 18:03:24 +0200

    Register the mylicorn thread into the  daemon, to avoid hanging on `terminate()`. Modified `collect_and_start_threads()` for that purpose, with an option to only collect threads, without overwritting existing references.

--- extensions/mylicorn/mylicorn.py
+++ extensions/mylicorn/mylicorn.py
@@ -8,15 +8,19 @@ Licorn extensions: mylicorn - http://docs.licorn.org/extensions/mylicorn
 
 """
 
-import os, urllib2
+import os, time, urllib2
 from threading import Thread
 
 from licorn.foundations           import exceptions, logging, settings, events, json
 from licorn.foundations.styles    import *
 from licorn.foundations.ltrace    import *
 from licorn.foundations.ltraces   import *
+from licorn.foundations.threads   import Event
+
 from licorn.foundations.base      import ObjectSingleton
 from licorn.foundations.constants import services, svccmds, distros
+
+from licorn.daemon.threads        import LicornJobThread
 
 from licorn.core                  import LMC
 from licorn.extensions            import LicornExtension
@@ -53,6 +57,10 @@ class MylicornExtension(ObjectSingleton, LicornExtension):
 	def __init__(self):
 		assert ltrace_func(TRACE_MYLICORN)
 		LicornExtension.__init__(self, name='mylicorn')
+
+		# advertise if we are connected via a standard event.
+		self.events.connected = Event()
+
 	def initialize(self):
 		""" The MyLicorn® extension is always available. At worst is it
 			disabled when there is no internet connexion, but even that
@@ -72,6 +80,12 @@ class MylicornExtension(ObjectSingleton, LicornExtension):
 		return True
 	@events.handler_method
 	def extension_mylicorn_check_finished(self, *args, **kwargs):
+
+		# Just trigger an authentication.
+		LicornEvent('extension_mylicorn_authenticate').emit()
+
+	@events.handler_method
+	def extension_mylicorn_authenticate(self, *args, **kwargs):
 		""" Authenticate ourselves on the central server.
 
 			.. note:: this callback is lanched after LMC.configuration is
@@ -79,6 +93,7 @@ class MylicornExtension(ObjectSingleton, LicornExtension):
 				used as the local server unique identifier, added to the
 				API key (if any).
 		"""
+
 		assert ltrace_func(TRACE_MYLICORN)
 
 		if LMC.configuration.system_uuid is None:
@@ -98,35 +113,98 @@ class MylicornExtension(ObjectSingleton, LicornExtension):
 			# default value, pointing to official My Licorn® webapp.
 			MY_LICORN_URI = 'http://my.licorn.org/json/'
 
-		myl = jsonrpc.ServiceProxy(MY_LICORN_URI)
+		self.service = jsonrpc.ServiceProxy(MY_LICORN_URI)
 
 		try:
 			api_key = settings.mylicorn.api_key
+
 		except:
 			api_key = None
 
 		try:
-			res = myl.authenticate(LMC.configuration.system_uuid, api_key)
-
-		except urllib2.HTTPError:
-			print_web_exception(e)
-			return
+			res = self.service.authenticate(LMC.configuration.system_uuid, api_key)
 
 		except Exception, e:
-			logging.exception('ERROR')
+
+			if isinstance(e, urllib2.HTTPError):
+				print_web_exception(e)
+
+			else:
+				logging.exception(_(u'{0}: error while authenticating; will retry '
+											u'in one hour.'), self.pretty_name)
+
+			LicornEvent('extension_mylicorn_authenticate').emit(delay=3600.0)
 			return
 
 		if res['result'] < 0:
 			# if authentication goes wrong, we won't even try to do anything
 			# more. Every RPC call needs authentication.
-			logging.warning(_(u'{0}: failed to authenticate (code: '
-							u'authenticate.{1}, message: {2})').format(
+			logging.warning(_(u'{0}: failed to authenticate, retrying in one '
+							u'hour (code: authenticate.{1}, message: {2})').format(
 								self.pretty_name, authenticate[res['result']],
 								res['message']))
+
+			LicornEvent('extension_mylicorn_authenticate').emit(delay=3600.0)
 			return
 
 		logging.info(_(u'{0}: sucessfully authenticated ourselves '
 						u'(code: authenticate.{1}, message: {2})').format(
 							self.pretty_name, authenticate[res['result']],
 							res['message']))
+
+		# Now that we are authenticated, we can report ourselves to our central
+		# server at a regular interval.
+		self.__start_updater_thread()
+	def __start_updater_thread(self):
+
+		if not self.events.connected.is_set():
+			self.events.connected.set()
+
+			self.threads.updater = LicornJobThread(
+								target=self.update_remote_informations,
+								# informations are updated every hour by default.
+								delay=3600,
+								tname='extensions.mylicorn.updater',
+								# first noop() is in 5 seconds.
+								time=(time.time()+5.0),
+								)
+
+			self.threads.updater.start()
+
+			self.licornd.collect_and_start_threads(collect_only=True)
+
+	def __stop_updater_thread(self):
+
+		if self.events.connected.is_set():
+			try:
+				self.threads.updater.stop()
+				del self.threads.updater
+
+			except:
+				logging.exception(_(u'{0}: exception while stopping updater thread'),
+																self.pretty_name)
+			self.events.connected.clear()
+	def update_remote_informations(self):
+		""" Method meant to be run from a Job Thread. It will run `noop()`
+			remotely, to trigger a remote informations update from the HTTP
+			request contents. From our point of view, this is just a kind of
+			"ping()".
+		"""
+		try:
+			self.service.noop()
+
+		except Exception, e:
+			if isinstance(e, urllib2.HTTPError):
+				print_web_exception(e)
+
+			else:
+				logging.exception(_(u'{0}: error while noop()\'ing; retrying '
+											u'in one hour.'), self.pretty_name)
+
+			# any [remote] exception will halt the current thread, and
+			# re-trigger a full pass of "authentication-then-regularly-update"
+			# after having waited one hour to make things settle.
+			self.__stop_updater_thread()
+
+			LicornEvent('extension_mylicorn_authenticate').emit(delay=3600.0)
 
