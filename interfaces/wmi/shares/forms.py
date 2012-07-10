2fdd8f58b25b2d2647a5cf501441e1f1affb16a1
Olivier Cortès <olive@licorn.org>
2012-07-10 11:11:49 +0200

    Password form UI enhancements.

--- interfaces/wmi/shares/forms.py
+++ interfaces/wmi/shares/forms.py
@@ -6,23 +6,19 @@ from django.utils.translation   import ugettext as _
 
 class AskSharePasswordForm(forms.Form):
 
-	password = forms.CharField( widget=forms.PasswordInput, label="Password")
+	password = forms.CharField(widget=forms.PasswordInput, label="Password")
 
 	def __init__(self, *args, **kwargs):
-		self.share = kwargs.get('share', None)
-		del kwargs['share']
+		self.share = kwargs.pop('share', None)
 		super(AskSharePasswordForm, self).__init__(*args, **kwargs)
 
 	def validate_password(self):
-		print "check_password ", self.data['password']
-		print self.share.check_password(self.data['password'])
-
 		if not self.share.check_password(self.data['password']):
-			raise forms.ValidationError(_('The password is not correct'))
+			raise forms.ValidationError(_('Incorrect password'))
+
 		return self.data['password']
 
 	def clean(self,*args, **kwargs):
-		print ">> validate"
 		super(AskSharePasswordForm, self).clean(*args, **kwargs)
-		
+
 		self.validate_password()
