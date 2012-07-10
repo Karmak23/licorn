a1da41225794289ffcd1f2cb4f2e722117bea81c
Olivier Cortès <olive@licorn.org>
2012-07-10 17:05:45 +0200

    JS/CSS cleanups.

--- interfaces/wmi/media/js/jquery.upload_dnd.js
+++ interfaces/wmi/media/js/jquery.upload_dnd.js
@@ -1,4 +1,4 @@
-/* 
+/*
 	* Add a div "upload_recap" for upload results
 	* add a div "classic_upload" if you need it, it will be bind to the same action as the drag and drop upload function
 
@@ -30,32 +30,32 @@ $(document).ready(function(){
     }, options);
 
     return this.each(function() {
-    
+
     	$(this).bind('drop', { 'settings' : settings, 'drag_target': this }, drop )
     	$(this).bind('dragover', stop_event )
-    
+
     	// bind the dragenter to apply a style when drag enter in the upload box
     	// NOTE : we use a counter, because in Chromium, a dragleave event is triggered when entering a child
     	$(this).bind('dragenter', { 'settings' : settings, 'drag_target': this }, drag_enter );
 
     	$(this).bind('dragleave', { 'settings' : settings, 'drag_target': this }, drag_leave);
 
-    	// apply default css
-    	$(this).addClass('upload_area_default');
-    	
-    	    	
+		// apply default css
+		$(this).addClass('upload-area');
+
     	// bind our events to the "normal" http file browser, if found
     	if ($('#classic_upload') != []) {
 			$('#classic_upload').change(function(event){
+
 				// get files
-				var files = event.target.files
+				var files = event.target.files;
 
 				if (settings.run_function != null) {
 					// the default action is overwritten
-					settings.run_function(files, settings)
+					settings.run_function(files, settings);
 				}
 				else {
-					do_upload(files, settings)
+					do_upload(files, settings);
 				}
 			});
 		}
@@ -66,78 +66,68 @@ $(document).ready(function(){
 var num_drag_event = 0;
 
 function drag_enter(event) {
-	settings = event.data.settings
-	target = $(event.data.drag_target)
+	settings = event.data.settings;
+	target   = $(event.data.drag_target);
 
 	num_drag_event++;
-    target.addClass('upload_area_hover');
+    target.addClass('upload-area-hover');
 
-    stop_event(event)
+    stop_event(event);
 }
 function drag_leave(event) {
-	settings = event.data.settings
-	target = $(event.data.drag_target)
+	settings = event.data.settings;
+	target   = $(event.data.drag_target);
 
 	num_drag_event--;
 	if (num_drag_event == 0) {
-		target.removeClass('upload_area_hover');
+		target.removeClass('upload-area-hover');
 	}
 
-	stop_event(event)
+	stop_event(event);
 }
 
-
-
 // http://code.google.com/p/chromium/issues/detail?id=106705
-// issue with dragleave qui se répete on hover:  http://bugs.jquery.com/ticket/11801
+// Issue with dragleave which repeats on hover:
+// http://bugs.jquery.com/ticket/11801
 
 function drop(event){
-	stop_event(event)
-	//console.log(">> drop function")
+	stop_event(event);
+
 	// get settings
-	settings = event.data.settings
+	settings = event.data.settings;
 
 	// get upload files from event
 	var files = event.dataTransfer.files;
-	////console.log('>>>> files ', files)
 
+	// OLIVE disable; why 2 calls??
 	stop_event(event);
+
 	// drag leave
-	$(event.data.drag_target).trigger('dragleave')
+	$(event.data.drag_target).trigger('dragleave');
 
 	if (settings.run_function != null) {
 		// the default action is overwritten
-		settings.run_function(files, settings)
+		settings.run_function(files, settings);
 	}
 	else {
-		do_upload(files, settings)
+		do_upload(files, settings);
 	}
-
-
 }
 
 function do_upload(files, settings) {
-	//console.log('\t >> do upload')
 
 	$.each(files, function(i, file) {
-		//console.log('tototototo')
 
-		//console.log(file)
-
-
-	  	// prepare the recap line
-	  	console.log($('#upload_recap'), $('#upload_recap') != [])
 	  	if ($('#upload_recap') != []) {
 			var recap_line = $(settings.recap_line)
-			$(recap_line).find('#recap_file_name').html(file.name)
-			$(recap_line).find('#recap_file_size').html(getReadableFileSizeString(file.size))
-			$(recap_line).addClass('upload-result')
-			$('#upload_recap').append(recap_line)			
+			$(recap_line).find('#recap_file_name').html(file.name);
+			$(recap_line).find('#recap_file_size').html(getReadableFileSizeString(file.size));
+			$(recap_line).addClass('upload-result');
+			$('#upload_recap').append(recap_line);
 		}
 
 	  	// prepare the error_div
 	  	var error_div = $('<center><img src="'+settings.error_image_url+'"><center>')
-
 
 		var can_continue = true;
 
@@ -147,21 +137,21 @@ function do_upload(files, settings) {
 	  		//console.log('UNKNOWN FILE TYPE')
 	  		recap_line
 		    	.find('#recap_file_progress').html('').append(error_div.attr('title', "Unknown file type"));
-		    recap_line.addClass('upload-result-error')
+		    recap_line.addClass('upload-result-error');
 
 		    can_continue = false;
 	  	}
-	  	else if( settings.file_size_max != null) {
-	  		console.log("max size set to ", settings.file_size_max)
+	  	else if(settings.file_size_max != null) {
+	  		//console.log("max size set to ", settings.file_size_max);
+
 	  		if (file.size > settings.file_size_max) {
 	  			recap_line
 					.find('#recap_file_progress').html('').append(error_div.attr('title', "File too big, maximun allowed "+getReadableFileSizeString(settings.file_size_max)));
-				recap_line.addClass('upload-result-error')
+				recap_line.addClass('upload-result-error');
 	  			can_continue = false;
 	  		}
 
 	  	}
-
 
 		if (can_continue) {
 
@@ -173,15 +163,20 @@ function do_upload(files, settings) {
 						var percentage = Math.round((e.loaded * 100) / e.total);
 						recap_line
 							.find('#recap_file_progress')
-								.html('<div class="progress progress-striped" style="display:\'inline\'"> <div class="bar" style="width: '+percentage+'%;">'+percentage+'%</div></div>')
-						recap_line.addClass('upload-result-success')
-						
-						// it succeed, show it and remove it after 5 seconds
-						setTimeout( function(){ recap_line.remove() }, 5000);
-						////console.log("Percentage loaded: ", percentage);
+								.html('<div class="progress progress-striped" style="display:\'inline\'"> <div class="bar" style="width: '+percentage+'%;">'+percentage+'%</div></div>');
+						recap_line.addClass('upload-result-success');
 
+						// Upload succeeded, show result and remove it
+						// after 5 seconds, else the result area will
+						// be crowded with old result.
+						setTimeout( function(){
+							recap_line.slideUp('slow', function(){
+								$(this).remove();
+							});
+						}, 5000);
 					}
-					stop_event(e)
+
+					stop_event(e);
 				}, false);
 			}
 
@@ -198,15 +193,12 @@ function do_upload(files, settings) {
 					stop_event(e)
 				}, false);
 
+			provider = function(){ return xhr; };
 
-
-			provider=function(){ return xhr; };
-			
 			var csrf_token = {};
 			if ($('input[name$="csrfmiddlewaretoken"]') != null) {
 				csrf_token = { csrfmiddlewaretoken : $('input[name$="csrfmiddlewaretoken"]').attr('value') }
 			}
-
 
 			var datas = $.extend( {
 				'file': file,			// post upload action
@@ -218,9 +210,6 @@ function do_upload(files, settings) {
 				d.append(key, value)
 			})
 
-			////console.log('d', d)
-			////console.log('settings', settings)
-
 			// Requete ajax pour envoyer le fichier
 			$.ajax({
 				url:settings.upload_action_url,
@@ -228,25 +217,22 @@ function do_upload(files, settings) {
 				data: d,
 				async: true,
 
-				xhr:provider,
-				processData:false,
-				contentType:false,
-				success:function(data) {
+				xhr: provider,
+				processData: false,
+				contentType: false,
+				success: function(data) {
 					settings.success_handler(data)
 				},
-				error:function(error){
-					recap_line.find('#recap_file_progress').html('').append(error_div.attr('title', error.statusText))
-					recap_line.addClass('upload-result-error')
+				error: function(error){
+					recap_line.find('#recap_file_progress').html('').append(error_div.attr('title', error.statusText));
+					recap_line.addClass('upload-result-error');
 					if (settings.error_handler != null) {
-						settings.error_handler()
+						settings.error_handler();
 					}
-					
 				}
 			});
-
 		}
 	});
-	
 }
 
 // prevent default event and its propagation
