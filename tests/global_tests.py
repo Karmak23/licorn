c5261c156a9f095cd79fabc47c82742c16e538bd
Olivier Cortès <olive@licorn.org>
2012-07-19 13:36:55 +0200

    fix #829; `extendedgroup_data_file` is no more available; a little more love for `get config`; fix an exception name.

--- tests/global_tests.py
+++ tests/global_tests.py
@@ -69,8 +69,7 @@ def test_get(context, testsuite):
 
 	commands = []
 
-	for category in [ 'config_dir', 'main_config_file',
-		'extendedgroup_data_file' ]:
+	for category in [ 'config_dir', 'main_config_file' ]:
 		for mode in [ '', '-s', '-b', '--bourne-shell', '-c', '--c-shell',
 			'-p', '--php-code' ]:
 			commands.append(GET + [ 'configuration', category, mode ])
