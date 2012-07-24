b99495dae4a62ef601607dd95854781e5135a014
Olivier Cortès <olive@licorn.org>
2012-07-24 14:37:05 +0200

    On the road to have a feature-complete `samba3` extension: - check / manage the shares-related `responsibles` global system group: a user member of any `rsp-*` is automatically made member of this group, which is needed to maintain fine grained samba permissions on some optional shares in complex setups. - check / manage the `machines` group: every samba machine account will automatically made member of this group. This depends on the `smb.conf` `add machine script` directive, but the group will always exists as long as the extension is enabled, and the directive is shipped in our example/default configuration file.

--- extensions/samba3/netlogon.py
+++ extensions/samba3/netlogon.py
@@ -50,72 +50,7 @@ stylize = styles.stylize
 
 pretty_name = stylize(ST_NAME, 'netlogon')
 
-smbext = LMC.extensions.samba3
-
-def script_start():
-	return '''
-@echo off
-rem
-rem {0}
-rem
-rem {1}
-rem {2}
-rem
-rem {3}
-rem {4}
-rem
-rem {5}
-echo.
-echo.
-echo.
-echo.
-echo       {6}
-echo.
-echo.
-echo.
-echo.
-echo {7}
-
-set servname={8}
-'''.format(
-		_(u'Licorn® netlogon script'),
-		_(u'WARNING: do not modify this script directly:'),
-		_(u'it has been auto-generated, and will be overwritten next time the user logs in.'),
-		_(u'If you would like to customize it, use templates from {0}').format(TEMPLATES_BASE_DIR),
-		_(u'for inspiration, and create new scripts in {0}.').format(CUSTOM_BASE_DIR),
-		_(u'For more information, visit http://docs.licorn.org/extensions/samba3'),
-		_('Hello {0} and welcome to {1}!').format(user_session_name, server_smbname),
-		_(u'{0} generated on {1}').format(script_filename, time.strftime('%c')),
-		server_smbname
-	).replace('\n', '\r')
-def script_build_levels():
-	""" Build a list of different script base names that we will try to
-		construct the user's final netlogon script. """
-
-	levels = [ '_base', client_smbname, server_smbname, user_domain ]
-
-	if user in admins:
-		levels.append('_admins')
-
-	if is_responsible:
-		levels.append('_responsibles')
-
-	else:
-		levels.append('_users')
-
-	# Groups will be sorted by quasi importance:
-	# at least system groups come before standard ones
-	levels.extend(g.name for g in sorted(user.groups, key=attrgetter('gid')))
-
-	levels.append(user.login)
-
-	return levels
-def script_out(msg):
-	# this function will write the script in a Windows-friendly way
-	script_buffer += '%s\r' % msg
-
-# ============================================================= Local variables
-MS_BASE_DIR           = os.path.join(LMC.configuration.home_base_path, 'windows')
+MS_BASE_DIR           = os.path.join(settings.defaults.home_base_path, 'windows')
 NETLOGON_BASE_DIR     = os.path.join(MS_BASE_DIR, 'netlogon')
 TEMPLATES_BASE_DIR    = os.path.join(NETLOGON_BASE_DIR, 'templates')
 CUSTOM_BASE_DIR       = os.path.join(NETLOGON_BASE_DIR, 'local')
@@ -123,134 +58,220 @@ USERS_PROFILES_DIR    = os.path.join(MS_BASE_DIR, 'profiles', 'Users')
 GROUPS_PROFILES_DIR   = os.path.join(MS_BASE_DIR, 'profiles', 'Groups')
 MACHINES_PROFILES_DIR = os.path.join(MS_BASE_DIR, 'profiles', 'Machines')
 
-user                  = LMC.users.by_login(kwargs.pop('user_login'))
-user_session_name     = kwargs.pop('user_session_name')
-user_domain           = kwargs.pop('user_domain')
-admins                = LMC.groups.by_name(settings.defaults.admin_group)
-licorn_wmi            = LMC.groups.by_name(settings.licornd.wmi.group)
 
-# The user is considered "responsible" of something if he is a member of
-# the licorn-wmi group, or if he is responsible of any standard group.
-is_responsible        = bool(user in licorn_wmi or [g for g in user.groups if g.is_responsible])
+def netlogon(*args, **kwargs):
 
-machine               = kwargs.pop('client_smbname')
-server_smbname        = kwargs.pop('server_smbname')
-script_filename       = os.path.join(NETLOGON_BASE_DIR, machine + '.cmd')
-script_levels         = script_build_levels()
-script_buffer         = script_start()
+	def script_start():
+		return '''
+	@echo off
+	rem
+	rem {0}
+	rem
+	rem {1}
+	rem {2}
+	rem
+	rem {3}
+	rem {4}
+	rem
+	rem {5}
+	echo.
+	echo.
+	echo.
+	echo.
+	echo       {6}
+	echo.
+	echo.
+	echo.
+	echo.
+	echo {7}
 
+	set servname={8}
+	'''.format(
+			_(u'Licorn® netlogon script'),
+			_(u'WARNING: do not modify this script directly:'),
+			_(u'it has been auto-generated, and will be overwritten next time the user logs in.'),
+			_(u'If you would like to customize it, use templates from {0}').format(TEMPLATES_BASE_DIR),
+			_(u'for inspiration, and create new scripts in {0}.').format(CUSTOM_BASE_DIR),
+			_(u'For more information, visit http://docs.licorn.org/extensions/samba3'),
+			_('Hello {0} and welcome to {1}!').format(user_session_name, server_smbname),
+			_(u'{0} generated on {1}').format(script_filename, time.strftime('%c')),
+			server_smbname
+		).replace('\n', '\r')
+	def script_build_levels():
+		""" Build a list of different script base names that we will try to
+			construct the user's final netlogon script. """
 
-# Windows® profiles priorities:
-#
-# - if the user is an administrator, his profile will be left untouched
-# - else:
-#	- if the machine has a fixed profile, use it
-#	- if not, if the group has a fixed profile, use it
-#	- if not, if the user has a fixed profile, use it
-#	- else, let Windows do whatever it wants
+		levels = [ '_base', client_smbname, server_smbname, user_domain ]
 
-overwriter_profile = None
+		if user in admins:
+			levels.append('_admins')
 
-if user not in admins:
-	for profile_path, profile_name in (
-							(MACHINES_PROFILES_DIR, machine)
-							(GROUPS_PROFILES_DIR, user.primaryGroup.name)
-							(USERS_PROFILES_DIR, user.login)
-						):
+		if is_responsible:
+			levels.append('_responsibles')
 
-	potential_overwriter = os.path.join(profile_path, profile_name)
+		else:
+			levels.append('_users')
 
-	if os.path.exists(potential_overwriter):
-		overwriter_profile = potential_overwriter
+		# Groups will be sorted by quasi importance:
+		# at least system groups come before standard ones
+		levels.extend(g.name for g in sorted(user.groups, key=attrgetter('gid')))
 
-		# the first one found takes precedence
-		break
+		levels.append(user.login)
 
-if potential_overwriter:
-	# WARNING: the `samba3` or `samba4` extension should be enabled,
-	# and Samba fully configured for this to work properly.
+		return levels
+	def script_out(msg):
+		# this function will write the script in a Windows-friendly way
+		script_buffer += '%s\r' % msg
+	def netlogon_profile():
 
-	# user.sambaHomeDirectory should be something
-	# like ~user/.samba/windows/ or ~user/windows
+		# Windows® profiles priorities:
+		#
+		# - if the user is an administrator, his profile will be left untouched
+		# - else:
+		#	- if the machine has a fixed profile, use it
+		#	- if not, if the group has a fixed profile, use it
+		#	- if not, if the user has a fixed profile, use it
+		#	- else, let Windows do whatever it wants
 
-	assert ltrace(TRACE_SAMBA3, '  Profile rewrite for user {0}, based on {1}',
-											user.login, potential_overwriter)
+		overwriter_profile = None
 
-	for thing_to_overwrite, remove_first in (
-											# TODO: localize the names
-											('Programmes', True),
-											('Menu Démarrer', True),
-											('Bureau', False)
-										):
+		if user not in admins:
+			for profile_path, profile_name in (
+									(MACHINES_PROFILES_DIR, machine)
+									(GROUPS_PROFILES_DIR, user.primaryGroup.name)
+									(USERS_PROFILES_DIR, user.login)
+				):
 
-		source      = os.path.join(potential_overwriter, thing_to_overwrite)
-		destination = os.path.join(user.sambaHomeDirectory, thing_to_overwrite)
+				potential_overwriter = os.path.join(profile_path, profile_name)
 
-		if remove_first:
-			try:
-				shutil.rmtree(destination)
+				if os.path.exists(potential_overwriter):
+					overwriter_profile = potential_overwriter
 
-			except (IOError, OSError), e:
-				if e.errno != errno.ENOENT:
-					logging.exception(_(u'{0}: exception while removing {1}, this '
-						u'could lead to inconsistencies in {2}\'s profile.'),
-							pretty_name, (ST_PATH, destination),
-							(ST_NAME, user.login))
+					# the first one found takes precedence
+					break
+
+		if potential_overwriter:
+			# WARNING: the `samba3` or `samba4` extension should be enabled,
+			# and Samba fully configured for this to work properly.
+
+			# user.sambaHomeDirectory should be something
+			# like ~user/.samba/windows/ or ~user/windows
+
+			assert ltrace(TRACE_SAMBA3, '  Profile rewrite for user {0}, based on {1}',
+													user.login, potential_overwriter)
+
+			for thing_to_overwrite, remove_first in (
+													# TODO: localize the names
+													('Programmes', True),
+													('Menu Démarrer', True),
+													('Bureau', False)
+												):
+
+				source      = os.path.join(potential_overwriter, thing_to_overwrite)
+				destination = os.path.join(user.sambaHomeDirectory, thing_to_overwrite)
+
+				if remove_first:
+					try:
+						shutil.rmtree(destination)
+
+					except (IOError, OSError), e:
+						if e.errno != errno.ENOENT:
+							logging.exception(_(u'{0}: exception while removing {1}, this '
+								u'could lead to inconsistencies in {2}\'s profile.'),
+									pretty_name, (ST_PATH, destination),
+									(ST_NAME, user.login))
+
+				try:
+					shutil.copy(source, destination)
+
+				except (OSError, IOError), e:
+					logging.exception(_(u'{0}: exception while copying {1} to {2}, '
+								u'this could lead to inconsistencies in {3}\'s profile.'),
+									pretty_name, (ST_PATH, source),
+									(ST_PATH, destination), (ST_NAME, user.login))
+
+				# NOTE: we don't enqueue this as a job, because permissions
+				# must be OK *before* the user logs in, else Windows® could
+				# fail to log him in if it can't access some dirs.
+				user._fast_aclcheck(destination, expiry_check=True)
+
+	def netlogon_script():
+
+		assert ltrace_func(TRACE_SAMBA3)
 
 		try:
-			shutil.copy(source, destination)
+			# A user script is always rewritten from scratch at every user login,
+			# to always be up-to-date regarding factory and customized scripts.
+			os.unlink(script_filename)
 
 		except (OSError, IOError), e:
-			logging.exception(_(u'{0}: exception while copying {1} to {2}, '
-						u'this could lead to inconsistencies in {3}\'s profile.'),
-							pretty_name, (ST_PATH, source),
-							(ST_PATH, destination), (ST_NAME, user.login))
+			if e.errno != errno.ENOENT:
+				logging.exception(_(u'{0}: exception while removing old script {1}, '
+								u'this could lead to inconsistencies in {2}\'s profile.'),
+									pretty_name, (ST_PATH, script_filename),
+									(ST_NAME, user.login))
 
-		#chown -R ${USER} Programmes "Menu Démarrer" Bureau
-		print '>> TODO: ackcheck the logon scripts.'
-		#workers.aclcheck_enqueue()
+		assert ltrace(TRACE_SAMBA3, '  Netlogon script {0} build start', script_filename)
 
-try:
-	# A user script is always rewritten from scratch at every user login,
-	# to always be up-to-date regarding factory and customized scripts.
-	os.unlink(script_filename)
+		for script_part in script_levels:
 
-except (OSError, IOError), e:
-	if e.errno != errno.ENOENT:
-		logging.exception(_(u'{0}: exception while removing old script {1}, '
-						u'this could lead to inconsistencies in {2}\'s profile.'),
-							pretty_name, (ST_PATH, script_filename),
-							(ST_NAME, user.login))
+			to_add = None
 
-assert ltrace(TRACE_SAMBA3, '  Netlogon script {0} build start', script_filename)
+			for script, var_prefix in (
+					# local customized script
+					(os.path.join(CUSTOM_BASE_DIR, script_part + '.cmd'), script_part),
 
-for script_part in script_levels:
+					# idem, but backward compatible
+					(os.path.join(TEMPLATES_BASE_DIR, script_part + '-local.cmd'), script_part + '-local'),
 
-	to_add = None
+					# last resort, a factory-shipped script
+					(os.path.join(TEMPLATES_BASE_DIR, script_part + '.cmd'), script_part),
+				):
+				if os.path.exists(script):
+					to_add = script
 
-	for script, var_prefix in (
-			# local customized script
-			(os.path.join(CUSTOM_BASE_DIR, script_part + '.cmd'), script_part),
+					# the first found takes precedence, others are not tested.
+					break
 
-			# idem, but backward compatible
-			(os.path.join(TEMPLATES_BASE_DIR, script_part + '-local.cmd'), script_part + '-local'),
+			if to_add:
+				assert ltrace(TRACE_SAMBA3, '	Netlogon build script add {1}', script)
 
-			# last resort, a factory-shipped script
-			(os.path.join(TEMPLATES_BASE_DIR, script_part + '.cmd'), script_part),
-		):
-		if os.path.exists(script):
-			to_add = script
+				script_out('set {0}_osver={0}_{1}'.format(var_prefix, client_arch))
+				script_out()
+				script_out('setlocal')
+				script_buffer += open(script).read().replace('\n', '\r')
+				script_out('endlocal')
 
-			# the first found takes precedence, others are not tested.
-			break
+		assert ltrace(TRACE_SAMBA3, '  Netlogon script {0} build end', script_filename)
+		assert ltrace_func(TRACE_SAMBA3, 1)
 
-	if to_add:
-		assert ltrace(TRACE_SAMBA3, '	Netlogon build script add {1}', script)
+	user                  = LMC.users.by_login(kwargs.pop('user_login'))
+	user_session_name     = kwargs.pop('user_session_name')
+	user_domain           = kwargs.pop('user_domain')
+	admins                = LMC.groups.by_name(settings.defaults.admin_group)
+	licorn_wmi            = LMC.groups.by_name(settings.licornd.wmi.group)
 
-		script_out('set {0}_osver={0}_{1}'.format(var_prefix, client_arch))
-		script_out()
-		script_out('setlocal')
-		script_buffer += open(script).read().replace('\n', '\r')
-		script_out('endlocal')
+	# The user is considered "responsible" of something if he is a member of
+	# the licorn-wmi group, or if he is responsible of any standard group.
+	is_responsible        = bool(user in licorn_wmi or [g for g in user.groups if g.is_responsible])
 
-assert ltrace(TRACE_SAMBA3, '  Netlogon script {0} build finished', script_filename)
+	machine               = kwargs.pop('client_smbname')
+	server_smbname        = kwargs.pop('server_smbname')
+
+	script_filename       = os.path.join(NETLOGON_BASE_DIR, machine + '.cmd')
+	script_levels         = script_build_levels()
+	script_buffer         = script_start()
+
+	netlogon_profile()
+	netlogon_script()
+
+__all__ = (
+	'netlogon',
+	'MS_BASE_DIR',
+	'NETLOGON_BASE_DIR',
+	'TEMPLATES_BASE_DIR',
+	'CUSTOM_BASE_DIR',
+	'USERS_PROFILES_DIR',
+	'GROUPS_PROFILES_DIR',
+	'MACHINES_PROFILES_DIR',
+	)
--- extensions/samba3/samba3.py
+++ extensions/samba3/samba3.py
@@ -11,15 +11,20 @@ Licorn extensions: samba3 - http://docs.licorn.org/extensions/samba3.html
 
 import os
 
-from licorn.foundations           import logging, process, events
+from licorn.foundations           import settings, logging, events, process
 from licorn.foundations.styles    import *
 from licorn.foundations.ltrace    import *
 from licorn.foundations.ltraces   import *
-from licorn.foundations.base      import ObjectSingleton, MixedDictObject, LicornConfigObject
-from licorn.foundations.constants import distros, services, svccmds, priorities
+from licorn.foundations.base      import ObjectSingleton, MixedDictObject, \
+											LicornConfigObject
+from licorn.foundations.constants import distros, services, svccmds, \
+											priorities, filters
 
-from licorn.core       import LMC
-from licorn.extensions import LicornExtension
+from licorn.core         import LMC
+from licorn.core.classes import only_if_enabled
+from licorn.extensions   import LicornExtension
+
+import netlogon
 
 class Samba3Extension(ObjectSingleton, LicornExtension):
 	""" Handle Samba3 the minimal way: just ensure that user accounts are in
@@ -40,29 +45,53 @@ class Samba3Extension(ObjectSingleton, LicornExtension):
 		self.controllers_compat = [ 'users' ]
 
 		# Path is the same on Ubuntu / Debian
-		self.paths.smbpasswd = '/usr/bin/smbpasswd'
+		self.paths.smbpasswd  = '/usr/bin/smbpasswd'
+		self.paths.smb_conf   = '/etc/samba/smb.conf'
+		self.paths.smb_daemon = '/usr/sbin/smbd'
 
 		self.data = LicornConfigObject()
+
+		self.groups              = LicornConfigObject()
+
+		# The administrator can change groups names if desired.
+		# Defaults are 'machines' and 'responsibles'.
+		self.groups.machines     = settings.get(
+										'extensions.samba3.groups.machines',
+										'machines')
+		self.groups.responsibles = settings.get(
+										'extensions.samba3.groups.responsibles',
+										'responsibles')
 
 		if LMC.configuration.distro in (distros.UBUNTU,
 										distros.LICORN,
 										distros.DEBIAN):
+			# TODO: when this extension will be turned into a ServiceExtension,
+			# we should check /etc/default/samba::RUN_MODE and don't do anything
+			# if it is 'inetd'.
 			self.paths.service_defaults = '/etc/default/samba'
+	def users_load(self, *args, **kwargs):
+		""" Nothing particular to do here. """
+		pass
 	def initialize(self):
 		""" Set :attr:`self.available` to ``True`` if smbpasswd is
 			found on the local system, else ``False``. """
 
 		assert ltrace_func(TRACE_SAMBA3)
 
-		if os.path.exists(self.paths.smbpasswd):
-			logging.info(_(u'{0}: extension enabled.').format(
-				stylize(ST_NAME, self.name)))
+		if os.path.exists(self.paths.smbpasswd) or (
+				os.path.exists(self.paths.smb_conf)
+				and os.path.exists(self.paths.smb_daemon)):
 
+			logging.info(_(u'{0}: extension enabled.').format(self.pretty_name))
 			self.available = True
+
 		else:
 			logging.info(_(u'{0}: extension disabled because {1} '
-					'not found.').format(stylize(ST_NAME, self.name),
-					stylize(ST_PATH, self.paths.smbpasswd)))
+							u' nor ({2} and {3}) not found.').format(
+								self.pretty_name,
+								stylize(ST_PATH, self.paths.smbpasswd),
+								stylize(ST_PATH, self.paths.smb_conf),
+								stylize(ST_PATH, self.paths.smb_daemon)))
 			self.available = False
 
 		return self.available
@@ -70,18 +99,51 @@ class Samba3Extension(ObjectSingleton, LicornExtension):
 		""" Currently, this method does nothing: this extension is so simple
 			that we don't do anything needing a check in it. """
 		assert ltrace_func(TRACE_SAMBA3)
+
+		group_descrs = {
+			self.groups.machines    : _('Windows® workstations members of the MS domain'),
+			self.groups.responsibles: _('Users responsible of at least one group'),
+			}
+		for group in self.groups:
+			if not LMC.groups.exists(name=group):
+				if batch or logging.ask_for_repair(_(u'{0}: group {1} must be '
+									u'created. Do it?').format(
+									self.pretty_name,
+									stylize(ST_NAME, self.group)),
+								auto_answer=auto_answer):
+
+					LMC.groups.add_Group(name=group,
+										description=group_descrs[group],
+										system=True, batch=batch)
+				else:
+					raise exceptions.LicornCheckError(_(u'{0}: group {1} must '
+										u'exist before continuing.').format(
+											self.pretty_name,
+											stylize(ST_NAME, group)))
+
+		self.__check_responsibles(batch=batch, auto_answer=auto_answer)
+
+		# TODO:
+		#	- check smb.conf
+		#	- check groups (conf contents and inclusions)
+		#	- check users
+
 		return True
+	def __check_responsibles(self, batch=False, auto_answer=None):
+
+		allresps = LMC.groups.by_name(self.groups.responsibles)
+
+		for group in LMC.groups.select(filters.RESPONSIBLE):
+			allresps.add_Users(group.members)
+
 	def is_enabled(self):
 		""" Always return the value of :attr:`self.available`, because we make
 			no difference between beiing available and beiing enable in this very
 			simple samba3 extension. """
 		assert ltrace_func(TRACE_SAMBA3)
 		return self.available
-	def users_load(self):
-		""" This method does nothing for now: nothing particular is necessary. """
-		assert ltrace_func(TRACE_SAMBA3)
-		return True
 	@events.handler_method
+	@only_if_enabled
 	def user_post_add(self, *args, **kwargs):
 		""" Create a caldavd user account and the associated calendar resource,
 			then write the configuration and release the associated lock.
@@ -113,9 +175,11 @@ class Samba3Extension(ObjectSingleton, LicornExtension):
 			return True
 
 		except:
-			logging.exception(_(u'{0}: Exception in user_post_add({1})'), (ST_NAME, self.name), (ST_LOGIN, user.login))
+			logging.exception(_(u'{0}: Exception in user_post_add({1})'),
+									self.pretty_name, (ST_LOGIN, user.login))
 			return False
 	@events.handler_method
+	@only_if_enabled
 	def user_post_change_password(self, *args, **kwargs):
 		""" Update the user's password in samba3. """
 
@@ -144,9 +208,11 @@ class Samba3Extension(ObjectSingleton, LicornExtension):
 
 			return True
 		except:
-			logging.exception(_(u'{0}: Exception in user_post_change_password({1})'), (ST_NAME, self.name), (ST_LOGIN, user.login))
+			logging.exception(_(u'{0}: Exception in user_post_change_password({1})'),
+									self.pretty_name, (ST_LOGIN, user.login))
 			return False
 	@events.handler_method
+	@only_if_enabled
 	def user_pre_del(self, *args, **kwargs):
 		""" delete a user and its resource in the caldavd accounts file, then
 			reload the service. """
@@ -176,16 +242,83 @@ class Samba3Extension(ObjectSingleton, LicornExtension):
 			return True
 
 		except:
-			logging.exception(_(u'{0}: Exception in user_pre_del({1})'), (ST_NAME, self.name), (ST_LOGIN, user.login))
+			logging.exception(_(u'{0}: Exception in user_pre_del({1})'),
+								c, (ST_LOGIN, user.login))
 			return False
 	@events.handler_method
-	def user_logged_in(self, *args, **kwargs):
+	@only_if_enabled
+	def group_post_add_user(self, *args, **kwargs):
+		""" Event handler that will add the user to the ``responsibles`` global
+			group if he was just added to any responsible group of the
+			system. """
 
-		user = kwargs.pop('user')
+		assert ltrace_func(TRACE_SAMBA3)
 
-		samba_params = dict(key.split('_', 1)[1], value
-								for key, value in kwargs.iteritems()
-									if key.startswith('smb_')
-										or key.startswith('samba_'))
+		group = kwargs.pop('group')
+		user  = kwargs.pop('user')
 
-		print '>> SMB USER LOGGED IN:', user
+		# We don't deal with system accounts.
+		# We are only interested in rsp-* groups.
+		if user.is_system or not group.is_responsible:
+			return True
+
+		allresps = LMC.groups.by_name(self.groups.responsibles)
+
+		try:
+			allresps.add_Users([user])
+
+		except:
+			logging.exception(_(u'{0}: Error while adding user {1} to '
+				u'group {2}'), self.pretty_name, user.login, allresps.name)
+
+			return False
+
+		return True
+	@events.handler_method
+	@only_if_enabled
+	def group_post_del_user(self, *args, **kwargs):
+		""" Event handler that will remove the user from the ``responsibles``
+			global group if he is not a member of any responsible group
+			anymore. """
+
+		assert ltrace_func(TRACE_SAMBA3)
+
+		group = kwargs.pop('group')
+		user  = kwargs.pop('user')
+
+		# We don't deal with system accounts.
+		# We are only interested in rsp-* groups.
+		if user.is_system or not group.is_responsible:
+			return True
+
+		# If the user is still responsible of any other group,
+		# he should be maintained as 'responsibles' member.
+		if [ g for g in user.groups if g.is_responsible ]:
+			return True
+
+		allresps = LMC.groups.by_name(self.groups.responsibles)
+
+		try:
+			allresps.del_Users([user])
+
+		except:
+			logging.exception(_(u'{0}: Error while removing user {1} from '
+				u'group {2}'), self.pretty_name, user.login, allresps.name)
+
+			return False
+
+		return True
+	@events.handler_method
+	@only_if_enabled
+	def user_logged_in(self, event, *args, **kwargs):
+
+		assert ltrace_func(TRACE_SAMBA3)
+
+		#samba_params = dict(key.split('_', 1)[1], value
+		#						for key, value in kwargs.iteritems()
+		#							if key.startswith('smb_')
+		#								or key.startswith('samba_'))
+
+		netlogon.netlogon(*event.args, **event.kwargs)
+
+__all__ = ('Samba3Extension', )
