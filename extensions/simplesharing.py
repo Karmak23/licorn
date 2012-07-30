14dfb2be69c84c37c643c81e96a729a663e9d5bc
Olivier Cortès <olive@licorn.org>
2012-07-30 11:51:58 +0200

    fix #849.

--- extensions/simplesharing.py
+++ extensions/simplesharing.py
@@ -479,7 +479,7 @@ class SimpleSharingUser(object):
 				logging.warning(_(u'User {0} does not accept simple '
 									u'shares, check skipped.').format(
 										stylize(ST_LOGIN, self.login)))
-	def list_shares(self):
+	def list_shares(self, share_hidden_dirs=False):
 		""" List a user shares, via a generator yielding every share found
 			in the user shares directory. """
 
@@ -488,6 +488,15 @@ class SimpleSharingUser(object):
 		with self.lock:
 			try:
 				for entry in os.listdir(self.shares_directory):
+					if entry.startswith('.') and not share_hidden_dirs:
+						# skip dotdirs. This is not done in the SimpleShare's
+						# `__init__()` because technically this is a perfectly
+						# valid case. We just don't share them *in Licorn®*.
+						#
+						# Anyone else could use the simplesharing facilities
+						# outside Licorn®, and share dotdirs if they want.
+						continue
+
 					try:
 						shares.append(SimpleShare(directory=os.path.join(
 												self.shares_directory, entry),
