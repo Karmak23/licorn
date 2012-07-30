ff3bb7864d74fb8366238306d407f88c9cda095e
Robin <robinlucbernet@gmail.com>
2012-07-30 21:45:00 +0200

    introduce the new machine edit template (using bootstrap), make edit url semantic

--- interfaces/wmi/machines/forms.py
+++ interfaces/wmi/machines/forms.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
-import os
 from django                       import forms
 from django.utils.translation     import ugettext_lazy as _
 
-from licorn.interfaces.wmi.libs  import utils
-from licorn.core                 import LMC
+from django                     import forms
+from django.utils.translation   import ugettext as _
+
+
+
+class MachineForm(forms.Form):
+
+    def __init__(self, tab=False, *args, **kwargs):
+		super(MachineForm, self).__init__(*args, **kwargs)
+		self.tab = tab
+		self.fields['machine_id']   = forms.CharField(label=_("Machine identifier"), initial='0000000', max_length=64, required=False)
+		self.fields['input2']   = forms.CharField(label=_("Input 2"), help_text='100 characters max.', max_length=64, required=False)
+		self.fields['input3']   = forms.CharField(label=_("Input 3"), help_text='100 characters max.', max_length=64, required=False)
+
+
+machine_form_blocks = {
+    'machine_id'              : ('general', u'General information'),
+    'input3'              : ('second', u'Second Part'),
+}
--- interfaces/wmi/machines/views.py
+++ interfaces/wmi/machines/views.py
@@ -20,6 +20,7 @@ from licorn.interfaces.wmi.libs.decorators import staff_only
 from licorn.interfaces.wmi.libs                import old_utils as w
 
 import wmi_data
+from forms  import MachineForm, machine_form_blocks
 
 @staff_only
 def main(request, sort="login", order="asc", select=None, **kwargs):
@@ -44,9 +45,30 @@ def scan(request, *args, **kwargs):
 	return HttpResponse('Processing network scan')
 
 def edit(request, mid, *args, **kwargs):
-	return HttpResponse(render_to_string('machines/edit.html', {
-		'machine' : LMC.machines.guess_one(mid)
-		}))
+	""" machine edit view.  """
+
+	edit_dict = {
+		'machine'     : LMC.machines.guess_one(mid),
+		'form'        : MachineForm(tab=False),
+		'form_blocks' : machine_form_blocks,
+	}
+
+	if request.is_ajax():
+		return render(request, 'machines/edit.html', edit_dict)
+
+	else:
+		return render(request, 'machines/index.html', {
+			'machines'        : utils.select('machines', default_selection=host_status.ONLINE),
+			'get_host_status_html' : wmi_data.get_host_status_html,
+			'get_host_os_html'     : wmi_data.get_host_os_html,
+			'get_host_type_html'   : wmi_data.get_host_type_html,
+			'edit_html'  : render_to_string('machines/edit.html', edit_dict)
+		})
+
+
+
+
+	return HttpResponse()
 
 def upgrade(request, mid, *args, **kwargs):
 	LMC.machines.guess_one(mid).do_upgrade()
