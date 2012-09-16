5bf4806f52acd481b609232aa90625cd28d35af7
Olivier Cortès <olive@licorn.org>
2012-09-16 21:44:05 +0200

    FIner detail in warning2 message causes.

--- core/backends/openldap.py
+++ core/backends/openldap.py
@@ -323,13 +323,20 @@ class OpenldapBackend(Singleton, UsersBackend, GroupsBackend):
 
 		assert ltrace_func(TRACE_OPENLDAP)
 
-		logging.progress(_(u'{0}: checking backend configuration…').format(self.pretty_name))
+		logging.progress(_(u'{0}: checking backend configuration…').format(
+															self.pretty_name))
 
 		# we always check system files, whatever.
 		self.check_system_files(batch, auto_answer)
 
-		if not self.available or settings.role == roles.CLIENT:
-			logging.warning2(_(u'{0:s}: backend not available, not checking.'))
+		if not self.available:
+			logging.warning2(_(u'{0}: backend not available, not '
+								u'checking.').format(self.pretty_name))
+			return
+
+		if settings.role == roles.CLIENT:
+			logging.warning2(_(u'{0}: not checking OpenLDAP server in CLIENT '
+								u'role.').format(self.pretty_name))
 			return
 
 		if process.whoami() != 'root' and not self.bind_as_admin:
