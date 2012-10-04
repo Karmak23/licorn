167a93ae945d93290afd74aa4656d27c61e05aae
Olivier Cortès <olive@licorn.org>
2012-10-04 11:39:50 +0200

    Move `squid` extension to its own subdir, we now have data to put there.

---
+++ extensions/squid/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Licorn extensions: squid - http://docs.licorn.org/extensions/squid
+
+"""
+
+# get the class here for the ExtensionsManager to find it.
+from squid import SquidExtension
+
