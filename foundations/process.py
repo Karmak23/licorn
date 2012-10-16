11777a917ba3932ee4d5043ae169fa463c6ec41a
Olivier Cortès <olive@licorn.org>
2012-10-16 10:34:28 +0200

    `foundations.process`: move the `rdiffbackup` method `_find_binary()` here, with the name `find_executable()`. We need this at different places, it is much better here in foundations. Refresh it to use `os.environ`.

--- foundations/process.py
+++ foundations/process.py
@@ -29,9 +29,11 @@ __all__ = ('daemonize', 'write_pid_file', 'use_log_file', 'set_name',
 	'get_process_cmdline', 'already_running', 'syscmd', 'execute',
 	'execute_remote', 'whoami', 'refork_as_root_or_die', 'fork_licorn_daemon',
 	'get_traceback', 'find_network_client_infos', 'pidof', 'pid_for_socket',
-	'thread_basic_info', 'executable_exists_in_path', 'cgroup', )
+	'thread_basic_info', 'find_executable', 'executable_exists_in_path',
+	'default_path', 'cgroup', )
 
-cgroup_host = False
+default_path = '/bin:/usr/bin:/usr/local/bin:/opt/bin:/opt/local/bin'
+cgroup_host  = False
 
 with open('/etc/mtab') as mtab:
 	for mline in mtab.readlines():
@@ -325,8 +327,8 @@ def execute(command, input_data='', dry_run=None):
 	except (OSError, IOError), e:
 		logging.warning2(_(u'{0}: exception "{1}" while trying to '
 								u'process.execute({2}).').format(
-									stylize(ST_NAME, current_thread().name), 
-									stylize(ST_ATTR, e), 
+									stylize(ST_NAME, current_thread().name),
+									stylize(ST_ATTR, e),
 									stylize(ST_COMMENT, ' '.join(command))))
 		raise
 def execute_remote(ipaddr, command):
@@ -537,6 +539,15 @@ def thread_basic_info(thread, as_string=False):
 				daemon=thread.daemon,
 				ident=thread.ident
 			)
+def find_executable(binary):
+	""" Return the path of a binary on the local system, or ``None`` if
+		not found in the :envvar:`PATH`. """
+
+	for syspath in os.environ.get('PATH', default_path).split(':'):
+		if os.path.exists(os.path.join(syspath, binary)):
+			return os.path.join(syspath, binary)
+
+	return None
 def executable_exists_in_path(filename, raise_message=None):
 	""" Check if a given exectable is found in $PATH and:
 
@@ -545,7 +556,7 @@ def executable_exists_in_path(filename, raise_message=None):
 		* else return ``True`` or ``False``, depending on the found result.
 	"""
 
-	for pathname in os.environ['PATH'].split(':'):
+	for pathname in os.environ.get('PATH', default_path).split(':'):
 		if os.path.exists(os.path.join(pathname, filename)):
 			return True
 
